09569b1bb5690bdbe93adea874d8740302aff0c1
YusufTaha <wintraven@gmail.com>
2018-01-21 21:37:39 -0500

    Cleanup + blueprint

--- Game.java
+++ Game.java
@@ -19,12 +19,13 @@ import javax.swing.Timer;
 
 /**
  * The game frame class
- *
  */
+// TODO Move keylistener to game panel
+// ？actionPerformed checking for game == null?
 public final class Game extends JFrame implements ActionListener, KeyListener
 {
-	private final Timer tmr; // swing timer
-	private final GamePanel game; // a gamepanel that controls the player component
+	private final Timer clock; // swing timer
+	private final GamePanel gamePanel; // a gamepanel that controls the player component
 
 	/**
 	 * CONSTRUCTOR
@@ -37,69 +38,79 @@ public final class Game extends JFrame implements ActionListener, KeyListener
 		setResizable(false);
 		setVisible(true);
 
-		tmr = new Timer(10, this); // listens to actions every 10 ms
-		tmr.start();               // starts the timer
+		clock = new Timer(10, this); // listens to actions every 10 ms
+		clock.start();			     // starts the timer
 
-		game = new GamePanel(); // creates the player's game panel
-		add(game); // adds the game panel to the Game JFrame
+		gamePanel = new GamePanel(); // creates the player's game panel
+		add(gamePanel); // adds the game panel to the Game JFrame
 
 		addKeyListener(this);
     }
 
 	/**
 	 * invoked whenever an action occurs
-	 * @param evt an actionevent
+	 * @param evt : an actionevent that occurred
 	 */
 	public void actionPerformed(ActionEvent evt)
 	{
-		if(game != null) {game.refresh(); game.repaint();}
+		if(gamePanel != null) {gamePanel.refresh(); gamePanel.repaint();}
 	}
 
 	/**
+	 * Curtains up! Center stage!
+	 * Instantiates the game frame object
+	 * which also instantiates the panel object
+	 */
+    public static void main(String...arguments) {Game frame = new Game();}
+
+	/**
 	 * ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯UNUSED⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯
-	 * invoked when a key is "typed"
-	 * @param keyevt an keyeventevent
+	 * invoked when a key is typed
+	 * @param keyevt : a KeyEvent that occured
 	 */
     public void keyTyped(KeyEvent keyevt) {}
 
 	/**
-	 * invoked when a key is "pressed"
-	 * @param keyevt an keyeventevent
+	 * invoked when a key is pressed
+	 * @param keyevt : a KeyEvent that occured
 	 */
-    public void keyPressed(KeyEvent keyevt) {game.setKey(keyevt.getKeyCode(),true);}
+    public void keyPressed(KeyEvent keyevt) {gamePanel.setKey(keyevt.getKeyCode(),true);}
 
 	/**
-	 * invoked when a key is "released"
-	 * @param keyevt an keyeventevent
+	 * invoked when a key is released
+	 * @param keyevt : a KeyEvent that occured
 	 */
-    public void keyReleased(KeyEvent keyevt) {game.setKey(keyevt.getKeyCode(),false);}
-
-
-    public static void main(String[] arguments) {Game frame = new Game();}
+    public void keyReleased(KeyEvent keyevt) {gamePanel.setKey(keyevt.getKeyCode(),false);}
 }
 
 class GamePanel extends JPanel
 {
 	private int playerX,playerY;
-	private boolean[] keys; // boolean array of the keys
-	private Image playerImg;
-	private Image playerBulletImg;
-	private Image enemyBulletImg;
+	private boolean[] keys = new boolean[KeyEvent.KEY_LAST+1]; // boolean array of the keys
 
+	// Image dependencies
+	private Image playerImg  	  = new ImageIcon("playerShip.png").getImage();
+	private Image playerBulletImg = new ImageIcon("playerBullet.png").getImage();
+	private Image enemyBulletImg  = new ImageIcon("enemyBullet.png").getImage();
+
+	/**
+	 * CONSTRUCTOR
+	 */
 	public GamePanel()
 	{
-		keys = new boolean[KeyEvent.KEY_LAST+1];
-		// backGround = new ImageIcon("OuterSpace.jpg").getImage();
-		playerImg = new ImageIcon("playerShip.png").getImage();
-		playerBulletImg = new ImageIcon("playerBullet.png").getImage();
-		enemyBulletImg = new ImageIcon("enemyBullet.png").getImage();
-
 	    playerX = 280;
         playerY = 735;
 		setSize(600,800);
 	}
 
-    public void setKey(int k, boolean v) {keys[k] = v;}
+	/**
+	 * @param key : the key in value on the keyboard
+	 * @param state : the bool state explained below
+	 * sets the state for a key on the keyboard
+	 * true  = pressed
+	 * false = released
+	 */
+    public void setKey(int key, boolean state) {keys[key] = state;}
 
 	public void refresh()
 	{
@@ -114,9 +125,11 @@ class GamePanel extends JPanel
 		// fire
 		if(keys[KeyEvent.VK_SPACE]) {}
 
-		// sheild
+		// sheild powerup
 
-		// increased movement
+		// timewarp powerup
+
+		// boost powerup
 
 	}
 
@@ -130,10 +143,10 @@ class GamePanel extends JPanel
 		// playerShip
 		g.setColor(Color.black);
 		g.fillRect(playerX,playerY,40,40);
+
 		// playerImg.paintIcon(this, g, playerX,playerY);
 		g.drawImage(playerImg, playerX, playerY, this);
 		g.drawImage(playerBulletImg, 90, 90, this);
 		g.drawImage(enemyBulletImg, 200, 90, this);
-
     }
 }
